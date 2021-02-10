ea127a02836338c781e4db8f87c3c185db0661e0
Sergey <popravkin.s@mail.ru>
2021-02-10 14:07:32 +0300

    fix baggs 14:07

--- middleware/routesConfig.js
+++ middleware/routesConfig.js
@@ -1,57 +1,15 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports.routersConfig = (application) => {
-    application.use(loginRouter);
-    application.use(signUpRouter);
-    application.use(logoutRouter)
-    application.use(mainRouter);
-    application.use(galleryRouter);
-    application.use(contactsRouter)
-    application.use(animalsRouter)
-const signUpRouter = require('../routes/auth')
-const loginRouter = require('../routes/login')
-const mainRouter = require("../routes/main");
-const animalsRouter = require("../routes/animals");
-const tariffsRouter = require("../routes/tariffs");
-const contactsRouter = require("../routes/contacts");
+  const authRouter = require('../routes/auth')
+  const loginRouter = require('../routes/login')
+  const mainRouter = require("../routes/main");
+  const animalsRouter = require("../routes/animals");
+  const tariffsRouter = require("../routes/tariffs");
+  const contactsRouter = require("../routes/contacts");
 
 
 module.exports.routesConfig = (application) => {
-  // application.use(loginRouter);
-  // application.use(signUpRouter);
+  application.use("/login",loginRouter);
+  application.use("/auth", authRouter);
   // application.use(logoutRouter)
   application.use(mainRouter);
   application.use("/tariffs", tariffsRouter);
