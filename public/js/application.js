7ceac0a0494d6f322057639781d2ed4bee5e904f
Ekateraina <ekateraina@mail.ru>
2021-02-11 10:48:37 +0300

    logout

--- public/js/application.js
+++ public/js/application.js
@@ -1,31 +1,31 @@
-const navList_links = document.querySelectorAll(".navList_link");
-async function staticPage(path){
+// const navList_links = document.querySelectorAll(".navList_link");
+// async function staticPage(path){
 
-    const response = await fetch(`${path}`);
+//     const response = await fetch(`${path}`);
    
-    const result = await response.json();
-    const resp = await fetch(`/template${path}.hbs`);
+//     const result = await response.json();
+//     const resp = await fetch(`/template${path}.hbs`);
    
 
-    const hbs = await resp.text();
-    const template = Handlebars.compile(hbs);
-    console.log(template());
+//     const hbs = await resp.text();
+//     const template = Handlebars.compile(hbs);
+//     console.log(template());
 
-    document.body.innerHTML += template();
-    ymaps.ready(init);
-}
-
-for (let btn of navList_links) {
-  btn.addEventListener("click", async (e) => {
-    e.preventDefault();
-    let path = btn.getAttribute("href");
-
-//     switch (path){
-//     case '/contacts':
-//         staticPage(path)
+//     document.body.innerHTML += template();
+//     ymaps.ready(init);
 // }
-if(path==='/contacts'){
-    staticPage(path)
-}
-  });
-}
+
+// for (let btn of navList_links) {
+//   btn.addEventListener("click", async (e) => {
+//     e.preventDefault();
+//     let path = btn.getAttribute("href");
+
+// //     switch (path){
+// //     case '/contacts':
+// //         staticPage(path)
+// // }
+// if(path==='/contacts'){
+//     staticPage(path)
+// }
+//   });
+// }
