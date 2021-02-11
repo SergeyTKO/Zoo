acddfd6c80253b2f1c6bff68cf1a608de52e369f
Ekateraina <ekateraina@mail.ru>
2021-02-11 17:30:16 +0300

    admin-CRUD(delete)

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const hbs = require('hbs')
-const methodOverride = require('method-override')
 
 const { routesConfig } = require('./middleware/routesConfig');
 
@@ -20,16 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.use(cookieParser())
-app.use(methodOverride('X-HTTP-Method-Override'))
 routesConfig(app)
-
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      const method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-  }));
 
 module.exports = app
