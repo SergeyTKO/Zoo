fbf0c122131f34e8eb12ff856d1251cfd8bf8df2
Ekateraina <ekateraina@mail.ru>
2021-02-11 18:15:41 +0300

    admin-CRUD(update)

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Animal = require('../models/animalModel')
+const { updateOne } = require('../models/tariffModel')
 const Tariff = require('../models/tariffModel')
 
 router.post('/animal', async function(req,res){
@@ -24,17 +25,34 @@ router.post('/tariff', async function(req,res){
     res.redirect('/admin')
 })
 
-router.put('/animal/:id', async function(req,res){
+router.get('/animal/:id', async function(req,res){
     const animal = await Animal.findById(req.params.id)
     res.render('admin/animalEdit', {animal})
 })
 
-router.get('/animal/:id', async function(req,res){
-    await Animal.deleteOne({_id: req.params.id})
+router.post('/animal/:id', async function(req,res) {
+    const {animalName, desc} = req.body
+    await Animal.updateOne({_id: req.params.id}, {name: animalName, desc})
     res.redirect('/admin')
 })
 
 router.get('/tariff/:id', async function(req,res){
+    const tariff = await Tariff.findById(req.params.id)
+    res.render('admin/tariffEdit', {tariff})
+})
+
+router.post('/tariff/:id', async function(req,res) {
+    const {tariffName, price} = req.body
+    await Tariff.updateOne({_id: req.params.id}, {name: tariffName, price})
+    res.redirect('/admin')
+})
+
+router.delete('/animal/:id', async function(req,res){
+    await Animal.deleteOne({_id: req.params.id})
+    res.redirect('/admin')
+})
+
+router.delete('/tariff/:id', async function(req,res){
     await Tariff.deleteOne({_id: req.params.id})
     res.redirect('/admin')
 })
