464f65c7710c24f50c5d39836af65a39b708e328
Ekateraina <ekateraina@mail.ru>
2021-02-12 15:11:41 +0300

    Initial

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const { routesConfig } = require('./middleware/routesConfig');
 
 mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0.ozvba.mongodb.net/test`, {useNewUrlParser: true, useUnifiedTopology: true});
 
+
 const app = express()
 app.set('view engine', 'hbs')
 hbs.registerPartials('./views/partials')
