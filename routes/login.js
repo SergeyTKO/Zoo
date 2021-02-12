90bcd543804f9688c813053c4a5af9277fca7d9d
Sergey <popravkin.s@mail.ru>
2021-02-12 10:01:57 +0300

    10:02

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ const User = require("../models/userModel");
 const privateKey = "-hdye-1ekdkd-d5sjsj-5";
 
 router.get("/", function (req, res) {
-  res.json();
+  res.render("login");
 });
 
 router.post("/", async function (req, res) {
@@ -14,9 +14,9 @@ router.post("/", async function (req, res) {
   let user;
   try {
     if (name.includes("@")) {
-      user = await User.findOne({ email: name.toLowerCase() });
+      user = await User.findOne({ email: name });
     } else {
-      user = await User.findOne({ username: name.toLowerCase() });
+      user = await User.findOne({ username: name });
     }
     if (user) {
       const validPassword = await bcrypt.compare(password, user.password);
